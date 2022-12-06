1bf263de1f582b1df54b89f9a1ec1567453a68b2
jamieomaguire <jamieolivermaguire@googlemail.com>
2022-12-06 15:13:26 +0000

    add lit example comp

--- src/pie-button.js
+++ src/pie-button.js
@@ -1,26 +1,60 @@
-import { LitElement, css, html } from 'lit';
-
-export class PieButton extends LitElement {
-    static properties = {
-
-    };
-
-    // Scoped styles
-    static styles = css`
+export class MyElement extends LitElement {
+    static get styles() {
+      return css`
         :host {
-            color: red;
+          display: block;
+          border: solid 1px gray;
+          padding: 16px;
+          max-width: 800px;
         }
-    `;
-
-    constructor () {
-        super();
-        // Declare reactive properties here
-
+      `;
     }
-
-    render () {
-        return html`<p>Hello world!</p>`;
+  
+    static get properties() {
+      return {
+        /**
+         * The name to say "Hello" to.
+         * @type {string}
+         */
+        name: {type: String},
+  
+        /**
+         * The number of times the button has been clicked.
+         * @type {number}
+         */
+        count: {type: Number},
+      };
     }
-};
+  
+    constructor() {
+      super();
+      this.name = 'World';
+      this.count = 0;
+    }
+  
+    render() {
+      return html`
+        <h1>${this.sayHello(this.name)}!</h1>
+        <button @click=${this._onClick} part="button">
+          Click Count: ${this.count}
+        </button>
+        <slot></slot>
+      `;
+    }
+  
+    _onClick() {
+      this.count++;
+      this.dispatchEvent(new CustomEvent('count-changed'));
+    }
+  
+    /**
+     * Formats a greeting
+     * @param name {string} The name to say "Hello" to
+     * @returns {string} A greeting directed at `name`
+     */
+    sayHello(name) {
+      return `Hello, ${name}`;
+    }
+  }
 
 customElements.define('pie-button', PieButton);
