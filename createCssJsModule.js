e7c28b17822b47b8ac150b698045deb429578813
jamieomaguire <jamieolivermaguire@googlemail.com>
2022-12-12 11:51:33 +0000

    formatting

--- createCssJsModule.js
+++ createCssJsModule.js
@@ -1,9 +1,7 @@
 import fs from 'fs';
 
-const cssString = fs.readFileSync('./src/generated/style.css').toString();
-
 const moduleTemplate = `export const styles = () => \`{{replace}}\`;`;
-
+const cssString = fs.readFileSync('./src/generated/style.css').toString();
 const cssStringNoComments = cssString.replace(/\/\*[\s\S]*?\*\/|\/\/.*/g,'');
 
 const module = moduleTemplate.replace('{{replace}}', cssStringNoComments);
