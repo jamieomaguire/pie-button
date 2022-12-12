8d092ad5fbc6586118f6c0b012ffdfb10e4dce59
jamieomaguire <jamieolivermaguire@googlemail.com>
2022-12-12 13:33:26 +0000

    add vue example

---
+++ example-vue/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <p>You clicked {{count}} times</p>
+    <pie-button @count-changed="incrementCount" id="btn" size="Large" type="primary">Button 1</pie-button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data () {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    incrementCount () {
+      this.count++;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
---
+++ example-vue/vue.config.js
@@ -0,0 +1,16 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => ({
+        ...options,
+        compilerOptions: {
+          // treat any tag that starts with ion- as custom elements
+          isCustomElement: tag => tag.startsWith('pie-')
+        }
+      }))
+  }
+})
